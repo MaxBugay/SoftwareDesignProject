4f39184149226d7b80832bcd0c3200b2b39f5d40
Max Bugay <MaxBugay1@gmail.com>
2017-11-26 23:41:55 -0500

    Main Menu added

--- GeometryWars/src/geometrywars/GeometryWars.java
+++ GeometryWars/src/geometrywars/GeometryWars.java
@@ -16,18 +16,35 @@ import javax.swing.*;
 public class GeometryWars extends JApplet {
     public static void main(String[] args) {
         JFrame field = new JFrame("Geometry Wars"); //New Window
-        PlayingField PlayingField = new PlayingField(); //New Panel
-        field.setContentPane(PlayingField);//Put panel in window
+        new PlayingField(); //New Panel
+        /*field.setContentPane(PlayingField); //Put panel in window
         field.setSize(1920,1080); //Size of Window
         field.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Exit
-        field.setVisible(true); //Visible Window
-        field.setBackground(Color.BLACK);
+        field.setVisible(true); //Visible Window*/
     }
-    public static class PlayingField extends JPanel { 
+    public static class PlayingField extends JPanel implements ActionListener{ 
         private int xPos, yPos;
         
-        PlayingField() {
-            setBackground(Color.BLACK);
+        PlayingField(){
+            JFrame f = new JFrame("Main Menu");
+            f.setSize(1920, 1080);
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            //setBackground(Color.BLACK);
+            JMenuBar jmb = new JMenuBar();
+            JMenu MainMenu = new JMenu("Main Menu");
+            JMenuItem jmiStart = new JMenu("Start Game");
+            JMenuItem jmiHighScore = new JMenu("High Scores");
+            JMenuItem jmiExit = new JMenu("Exit");
+            MainMenu.add(jmiStart);
+            MainMenu.add(jmiHighScore);
+            MainMenu.add(jmiExit);
+            jmb.add(MainMenu);
+            jmiStart.addActionListener(this);
+            jmiHighScore.addActionListener(this);
+            jmiExit.addActionListener(this);
+            f.setJMenuBar(jmb);
+            f.setVisible(true);
+            //addKeyListener((KeyListener) this);
         }
         
         public void fieldComponent(Graphics g) {
@@ -48,5 +65,10 @@ public class GeometryWars extends JApplet {
         public void setY {
             yPos = this.x;
         }*/
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            String comStr = ae.getActionCommand();
+        }
     }
 }
