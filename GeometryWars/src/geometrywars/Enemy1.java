b725bb389f932345df30bffda956d5e495555e5a
Saif <Saif@Saif-PC>
2017-12-02 23:48:00 -0500

    Enemy Units Spawns Added

--- GeometryWars/src/geometrywars/Enemy1.java
+++ GeometryWars/src/geometrywars/Enemy1.java
@@ -10,17 +10,34 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.RenderingHints;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JComponent;
+import javax.swing.JPanel;
 
 /**
  *
  * @author Saif
  */
-public class Enemy1 implements EnemyUnit{
+public class Enemy1 extends JPanel implements EnemyUnit, Runnable {
 
+    private int xPosition;
+    private int yPosition;
+    private int playerX, playerY;
     Graphics g;
     private int HP = 0;
+    
     public Enemy1() {
+        xPosition = (int)(Math.random() * (1080));
+        yPosition = (int)(Math.random() * (1080));
+        onCreate();
+    }
+    
+    public Enemy1(int xPos, int yPos) {
+        xPosition = xPos;
+        yPosition = yPos;
+        this.playerX = playerX;
+        this.playerY = playerY;
         onCreate();
     }
 
@@ -33,19 +50,34 @@ public class Enemy1 implements EnemyUnit{
     @Override
     public void onCreate() {
         HP = 1;
-        //paintComponent(g);
+        Thread t = new Thread(this);
+        //t.start();
     }
-
-    /*@Override
+    
+    @Override
+    public void run() {
+    }
+    
+    @Override
     public void paintComponent(Graphics g) {
-        super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         g.setColor(Color.GREEN);
-        g.drawOval(500, 500, 15, 15);
-        g.fillOval(500, 500, 15, 15);
-    }*/
+        g.drawOval(xPosition, yPosition, 15, 15);
+        g.fillOval(xPosition, yPosition, 15, 15);
+        while (HP > 0) {
+            System.out.println("Memes");
+            if (playerX > xPosition)
+                xPosition += 5;
+            else
+                xPosition -=5;
+            if (playerY > yPosition)
+                yPosition += 5;
+            else
+                yPosition -= 5;
+        }
+    }
     
     @Override
     public void onDeath() {
@@ -54,6 +86,22 @@ public class Enemy1 implements EnemyUnit{
 
     public int getHP() {
         return HP;
+    }
+
+    public int getxPosition() {
+        return xPosition;
+    }
+
+    public int getyPosition() {
+        return yPosition;
+    }
+
+    public void setxPosition(int xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public void setyPosition(int yPosition) {
+        this.yPosition = yPosition;
     }
     
     
--- GeometryWars/src/geometrywars/GeometryWars.java
+++ GeometryWars/src/geometrywars/GeometryWars.java
@@ -12,19 +12,27 @@ package geometrywars;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
 
 public class GeometryWars extends JApplet {
-    public static class PlayingField extends JPanel implements ActionListener{ 
+    public static class PlayingField extends JPanel implements ActionListener { 
         public int xPos = 540, yPos = 540;
+        int i;
+        private int[] enemyX = new int[10];
+        private int[] enemyY = new int[10];
         private static final int PREF_W = 1080, PREF_H = 1080, SHIP_W = 15;;
+        private Enemy1[] triangle = new Enemy1[10];
         private JMenuBar jmb;
         private JMenu jmMainMenu;
         private JMenuItem jmiStart;
         private JMenuItem jmiHighScore;
         private JMenuItem jmiExit;
         private Font font = new Font("Arial", Font.ITALIC, 30);
-        
+        private boolean secondWave = false;
+        private boolean setSpawns = true;
         PlayingField(){
             jmb = new JMenuBar();
             jmMainMenu = new JMenu("Main Menu");
@@ -57,7 +65,13 @@ public class GeometryWars extends JApplet {
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
             this.setBackground(Color.BLUE);
-            
+            if (setSpawns) {
+                for (int x = 0; x < 10; x++) {
+                    enemyX[x] = (int)(Math.random() * (1080));
+                    enemyY[x] = (int)(Math.random() * (1080));
+                }
+                setSpawns = false;
+            }
             Graphics2D g2 = (Graphics2D) g;
             g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
             g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
@@ -68,18 +82,23 @@ public class GeometryWars extends JApplet {
             pentagonShip = new ImageIcon("PentagonShip.png").getImage();
             octagonShip = new ImageIcon("OctagonShip.png").getImage();
             g.drawString("Test", 100, 200);
-            
             g.drawImage(playerShip, SHIP_W, SHIP_W, this);
             g.setColor(Color.CYAN);
             g.drawOval(xPos, yPos, SHIP_W, SHIP_W);
             g.fillOval(xPos, yPos, SHIP_W, SHIP_W);
             int rand1, rand2;
-            for (int i = 0; i < 10; i++) {
-                rand1 = (int)(Math.random() * (1080));
-                rand2 = (int)(Math.random() * (1080));
-                g.setColor(Color.yellow);
-                g.drawOval(rand1, rand2, SHIP_W, SHIP_W);
-                g.fillOval(rand1, rand2, SHIP_W, SHIP_W);
+            g.setColor(Color.yellow);
+            for (int j = 0; j < 5; j++) {
+                g.drawOval(enemyX[j], enemyY[j], SHIP_W, SHIP_W);
+                    g.fillOval(enemyX[j], enemyY[j], SHIP_W, SHIP_W);
+            }
+            
+            if (secondWave) {
+                g.setColor(Color.GREEN);
+                for (int i = 5; i < 10; i++) {
+                    g.drawOval(enemyX[i], enemyY[i], SHIP_W, SHIP_W);
+                    g.fillOval(enemyX[i], enemyY[i], SHIP_W, SHIP_W);
+                }
                 
             }
             
@@ -100,14 +119,42 @@ public class GeometryWars extends JApplet {
                     public void actionPerformed(ActionEvent evt) {
                     int newX = xPos + dir.getxTrans();
                     int newY = yPos + dir.getyTrans();
-
                     newX = Math.min(newX, PREF_W - 2 * SHIP_W);
                     newX = Math.max(newX, SHIP_W);
                     newY = Math.min(newY, PREF_H - 2 * SHIP_W);
                     newY = Math.max(newY, SHIP_W);
-
+                    for (int j = 0; j < 10; j++) {
+                        triangle[j] = new Enemy1(enemyX[j], enemyY[j]);
+                    }
+                    
+                    for (int i = 0; i < 10; i++) {
+                        if (xPos >= enemyX[i]) {
+                            enemyX[i] += 3;
+                            triangle[i].setxPosition(enemyX[i]);
+                        }
+                        else {
+                            enemyX[i] -= 3;
+                            triangle[i].setxPosition(enemyX[i]);
+                        }
+                        if (yPos >= enemyY[i]) {
+                            enemyY[i] += 3;
+                            triangle[i].setyPosition(enemyY[i]);
+                        }
+                        else {
+                            enemyY[i] -= 3;
+                            triangle[i].setyPosition(enemyY[i]);
+                        }
+                    }
+                    
                     xPos = newX;
                     yPos = newY;
+                    for (int i = 0; i < 10; i++) {
+                        if (xPos >= enemyX[i] - 3 && xPos <= enemyX[i] + 3) 
+                            if (yPos >= enemyY[i] - 3 && yPos <= enemyY[i] + 3) {
+                                System.out.println("you died kid");
+                                secondWave = true;
+                            }
+                    }
                     repaint();
                     }
                 });
@@ -175,17 +222,49 @@ public class GeometryWars extends JApplet {
                 default:
                     break;
             }
-                
         }
+        /*public void enemy() {
+            Thread t = new Thread(this);
+            t.start();
+            Thread w = new Thread(this);
+            w.start();
+        }
+        @Override
+        public void run() {
+            Enemy1 triangle = new Enemy1(enemy1X, enemy1Y);
+                   while (triangle.getHP() > 0) {
+                       if (triangle.getxPosition() > xPos){
+                       enemy1X += 5;
+                       triangle.setxPosition(enemy1X);
+                   }
+                   else {
+                       enemy1X -= 5;
+                       triangle.setxPosition(enemy1X);
+                   }
+                   if (triangle.getyPosition() > yPos) {
+                       enemy1Y += 5;
+                       triangle.setyPosition(enemy1Y);
+                   }
+                   else {
+                       enemy1Y -= 5;
+                       triangle.setyPosition(enemy1Y);
+                   }
+                   repaint();
+                       //System.out.println(this.getxPos());
+                }
+        }*/
     }
+    
+    
+    
     public static void main(String[] args) {
+        PlayingField g = new PlayingField();
             SwingUtilities.invokeLater(new Runnable() {
                 @Override
                 public void run() {
-                    PlayingField.createField();
+                    g.createField();
                 }
             });
-            Enemy1 triangle = new Enemy1();
-            
+            //g.enemy();            
         }
 }
