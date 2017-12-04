bdc60ad5cbb72c1856a0b3e25092e54288e181ba
Max Bugay <MaxBugay1@outlook.com>
2017-12-04 15:48:48 -0500

    Setting up fire tests

--- GeometryWars/test/geometrywars/GeometryWarsTest.java
+++ GeometryWars/test/geometrywars/GeometryWarsTest.java
@@ -5,8 +5,8 @@
  */
 package geometrywars;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import geometrywars.GeometryWars.PlayingField;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -16,29 +16,73 @@ import static org.junit.Assert.*;
  */
 public class GeometryWarsTest {
     
+    PlayingField pf;
+    int x, y;
+    
+    @Before
+    public void setUp() {
+        pf = new PlayingField();
+        pf.setxPos(540);
+        pf.setyPos(540);
+        pf.firingDown = false;
+        pf.firingUp = false;
+        pf.firingLeft = false;
+        pf.firingRight = false;
+        x = pf.getxPos();
+        y = pf.getyPos();
+    }
+    
     @Test
-    public void GeometryWarsTest() {
-       
+    public void isFiringDown() {
+        pf.fireDown();
+        assertTrue(pf.firingDown);
     }
-    
-    /*@BeforeClass
-    public static void setUpClass() {
+    @Test
+    public void isFiringUp() {
+        pf.fireUp();
+        assertTrue(pf.firingUp);
     }
-    
-    @AfterClass
-    public static void tearDownClass() {
+    @Test
+    public void isFiringLeft() {
+        pf.fireLeft();
+        assertTrue(pf.firingLeft);
+    }
+    @Test
+    public void isFiringRight() {
+        pf.fireRight();
+        assertTrue(pf.firingRight);
+    }
+    @Test
+    public void isMoveUp() {
+        pf.moveUp();
+        assertEquals(pf.yPos,y+10);
+    }
+    @Test
+    public void isMoveDown() {
+        pf.moveDown();
+        assertEquals(pf.yPos,y-10);
+    }
+    @Test
+    public void isMoveLeft() {
+        pf.moveLeft();
+        assertEquals(pf.xPos,x-10);
+    }
+    @Test
+    public void isMoveRight() {
+        pf.moveRight();
+        assertEquals(pf.xPos,x+10);
     }
 
     /**
      * Test of main method, of class GeometryWars.
      */
-    @Test
+    /*@Test
     public void testMain() {
         System.out.println("main");
         String[] args = null;
         GeometryWars.main(args);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
-    }
+    }*/
     
 }
