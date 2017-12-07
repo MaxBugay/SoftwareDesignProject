3d5750404ff1a153b12cc62a50cd83fd3108c58f
Saif <Saif@Saif-PC>
2017-12-07 03:06:36 -0500

    4 Directional shooting and enemy deaths implemented

--- GeometryWars/src/geometrywars/Enemy1.java
+++ GeometryWars/src/geometrywars/Enemy1.java
@@ -19,7 +19,7 @@ import javax.swing.JPanel;
  *
  * @author Saif
  */
-public class Enemy1 extends JPanel implements EnemyUnit, Runnable {
+public class Enemy1 extends JPanel implements EnemyUnit {
 
     private int xPosition;
     private int yPosition;
@@ -49,35 +49,9 @@ public class Enemy1 extends JPanel implements EnemyUnit, Runnable {
 
     @Override
     public void onCreate() {
-        HP = 1;
-        Thread t = new Thread(this);
-        //t.start();
+        this.HP = 1;
     }
-    
-    @Override
-    public void run() {
-    }
-    
-    @Override
-    public void paintComponent(Graphics g) {
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        g.setColor(Color.GREEN);
-        g.drawOval(xPosition, yPosition, 15, 15);
-        g.fillOval(xPosition, yPosition, 15, 15);
-        while (HP > 0) {
-            System.out.println("Memes");
-            if (playerX > xPosition)
-                xPosition += 5;
-            else
-                xPosition -=5;
-            if (playerY > yPosition)
-                yPosition += 5;
-            else
-                yPosition -= 5;
-        }
-    }
+
     
     @Override
     public void onDeath() {
